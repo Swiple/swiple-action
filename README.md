0c73d2073da5375f615994950881fc0de9b1a719
Kenton Parton <kknoxparton@gmail.com>
2023-04-14 10:03:05 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# swiple-action
- Automatically validate datasets, poll task status, and display validation results in a GitHub pull request.
+# Swiple Dataset Validation Action
+
+This GitHub action automatically validates datasets, polls task status, and displays validation results in a GitHub pull request. It is designed to work with Swiple's dataset validation API.
+
+## Features
+
+- Authenticate with the Swiple API using provided credentials
+- Run dataset validation and obtain a task ID
+- Poll the task status until it reaches a final state (SUCCESS, FAILURE, or ERROR)
+- Retrieve the most recent validation results for a specific dataset ID
+- Display validation results as a table in the GitHub pull request
+
+## Usage
+
+To use this action, you need to add it as a step in your GitHub Actions workflow file (`.github/workflows/main.yml` or similar). Here's an example workflow:
+
+```yaml
+name: Dataset Validation
+
+on: [pull_request]
+
+jobs:
+  dataset_validation:
+    runs-on: ubuntu-latest
+
+    steps:
+    - name: Checkout
+      uses: actions/checkout@v2
+
+    - name: Swiple Dataset Validation
+      uses: your_github_username/swiple-action@v1
+      with:
+        api_base_url: ${{ secrets.API_BASE_URL }}
+        client_id: ${{ secrets.CLIENT_ID }}
+        client_secret: ${{ secrets.CLIENT_SECRET }}
+        username: ${{ secrets.USERNAME }}
+        password: ${{ secrets.PASSWORD }}
+        dataset_id: 'your_dataset_id'
+```
---
+++ validate_dataset.py
@@ -0,0 +1,161 @@
+import os
+import time
+import requests
+
+from datetime import datetime
+from github import Github
+from zoneinfo import ZoneInfo
+
+
+api_base_url = os.environ['API_BASE_URL']
+username = os.environ['USERNAME']
+password = os.environ['PASSWORD']
+dataset_id = os.environ['DATASET_ID']
+time_zone = os.environ.get("TIME_ZONE", "US/Central")
+
+session = requests.Session()
+
+
+def authenticate():
+    url = f"{api_base_url}/auth/login"
+    data = {
+        'grant_type': 'password',
+        'username': username,
+        'password': password,
+    }
+    response = session.post(url, data=data)
+    response.raise_for_status()
+
+
+def get_dataset(key):
+    url = f"{api_base_url}/datasets/{key}"
+    response = session.get(url)
+    response.raise_for_status()
+    return response.json()
+
+
+def validate_dataset(key):
+    url = f"{api_base_url}/datasets/{key}/validate"
+    response = session.post(url)
+    response.raise_for_status()
+    return response.json()['task_id']
+
+
+def get_task(task_id):
+    url = f"{api_base_url}/tasks/{task_id}"
+    response = session.get(url)
+    response.raise_for_status()
+    return response.json()
+
+
+def poll_task_status(task_id):
+    while True:
+        task = get_task(task_id)
+        status = task['status']
+        if status == 'SUCCESS':
+            return task
+        elif status in ('FAILURE', 'ERROR'):
+            raise Exception(f"Task failed with status {status}: {task['result']}")
+        else:
+            time.sleep(5)
+
+
+def get_validations(dataset_id):
+    url = f"{api_base_url}/expectations?dataset_id={dataset_id}&include_history=true&enabled=true&asc=false"
+    response = session.get(url)
+    response.raise_for_status()
+    return response.json()
+
+
+def process_validation_result(result: dict, result_type: str):
+    if result_type == "column_map_expectation":
+        result_value = f'{round(100 - result["result"]["unexpected_percent"], 5)}%'
+    elif result_type == "column_aggregate_expectation":
+        result_value = round(result["result"]["observed_value"], 5)
+    elif result_type == "expectation":
+        if "observed_value" in result["result"]:
+            result_value = round(result["result"]["observed_value"], 5)
+        elif "observed_value_list" in result["result"]:
+            result_value = '-'
+        else:
+            raise ValueError(f"{result_type} not implemented. Data")
+    else:
+        raise ValueError(f"{result_type} not implemented. Data")
+
+    return result_value
+
+
+def format_run_time(date_string: str):
+    dt = datetime.fromisoformat(date_string.replace("Z", "+00:00"))
+
+    # Convert the datetime object to US Central Time
+    desired_timezone = ZoneInfo(time_zone)
+    localized_dt = dt.astimezone(desired_timezone)
+
+    # Format the datetime object for easy viewing
+    formatted_date = localized_dt.strftime("%Y-%m-%d %I:%M:%S %p %Z")
+    return formatted_date
+
+
+def post_pr_comment(repo_name, pr_number, message):
+    github_token = os.environ['GITHUB_TOKEN']
+    g = Github(github_token)
+    repo = g.get_repo(repo_name)
+    pr = repo.get_pull(pr_number)
+    pr.create_issue_comment(message)
+
+
+def main():
+    authenticate()
+    dataset = get_dataset(dataset_id)
+    task_id = validate_dataset(dataset_id)
+    task = poll_task_status(task_id)
+    validations = get_validations(dataset_id)
+
+    validation_results = []
+
+    for expectation in validations:
+        if expectation.get("validations")[0]['exception_info']['exception_message']:
+            status = '❗'
+        else:
+            status = '✅' if expectation.get("validations")[0]['success'] else '❌'
+
+        result = {
+            "expectation_type": expectation.get("expectation_type"),
+            "success": status,
+            "result_value": process_validation_result(expectation.get("validations")[0], expectation["result_type"]),
+            "documentation": expectation.get("documentation"),
+            "column": expectation.get('kwargs', {}).get("column", ''),
+            "kwargs": expectation.get("kwargs"),
+        }
+        validation_results.append(result)
+
+    # Format the validation results as a table
+    table_header = "| Column | Expectation | Success | Result | Documentation |\n| --- | --- | --- | --- | --- |\n"
+    table_rows = [
+        f"| {result['column']} | {result['expectation_type']} | {result['success']} | {result['result_value']} | {result['documentation']} |"
+        for result in validation_results
+    ]
+
+    table = table_header + "\n".join(table_rows)
+
+    engine = dataset["engine"]
+    datasource_name = dataset["datasource_name"]
+    database = dataset["database"]
+    dataset_name = dataset["dataset_name"]
+    run_time = format_run_time(validations[0].get("validations")[0]["run_time"])
+
+    overview_header ="| Engine | Datasource Name | Database | Dataset Name | Run Time |\n|---|---|---|---|---|\n"
+    overview_table_row = f"|{engine}|{datasource_name}|{database}|{dataset_name}|{run_time}|"
+    overview_table = overview_header + overview_table_row
+
+    # Post the validation results as a PR comment
+    repo_name = os.environ["GITHUB_REPOSITORY"]
+    pr_number = int(os.environ["INPUT_PR_NUMBER"])
+    markdown = f"## Validation Report\n---\n{overview_table}\n\n## Results\n\n{table}"
+    print(markdown)
+    post_pr_comment(repo_name, pr_number, markdown)
+
+
+if __name__ == "__main__":
+    main()
